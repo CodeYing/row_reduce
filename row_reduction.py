73e6f79356b4c42d2ef0a0efbf633c12ee406c50
yingyingwu <yingyingwu0815@gmail.com>
2018-05-09 01:04:27 -0700

    (WIP) Created a matrix class to host all the functions

--- row_reduction.py
+++ row_reduction.py
@@ -1,70 +1,68 @@
 """
-Function that take a m(row)* n(column) matrix and
-row reduce to reduced row echelon form
+A matrix class that takes a m(row)* n(column) matrix and has
+a function to row reduce the matrix to Reduced Row Echelon Form
+Example:
+mtx1 = Matrix([[1, 2], [3, 4]])
 """
-import operator
 
-def row_reduce(matrix):
-    row = len (matrix)
-    col = len (matrix[0])
-    # for each row in matrix, perform row reduction
-    for x in range (0,row):
-        while (pos_is_zero(matrix, x)):
-            matrix= move_to_last(matrix, x)
-        matrix = reduce(x, matrix)
-    return matrix
+class Matrix:
+    def __init__(self, values):
+        self.n = len(values)
+        self.m = len(values[0])
+        self.values = []
+        for row in values:
+            if (len(row) != self.m):
+                raise Exception('Col mismatch')
+        self.values = values
 
-def pos_is_zero(matrix, x):
-    """
-    check if the x row and x column position is 0
-    """
-    row = matrix[x]
-    return (row[x] == 0)
+    def row_reduce(self):
+        # row reduce a matrix to its RREF
+        for pivot_row in range (0,self.m):
+            cur_col_index = pivot_row
+            for cur_row_index in range (pivot_row, self.m):
+                cur_col_index = find_nonzero(cur_row_index, cur_col_index)
+                if cur_col_index < self.m:
+                    scale(cur_row_index, cur_col_index)
+                    make_zero(cur_row_index, cur_col_index)
+
+    def find_nonzero(self, cur_row_index, cur_col_index):
+        """
+        Given the current row index and current column index,
+        select a nonzero entry in the pivot columnas a pivot,
+        swap rows to move this entry into the pivot position
+        if necessary.
+        Returns the pivot column index
+        """
+        while (cur_col_index < self.n):
+            for r in range (cur_row_index, self.m):
+                row = self.values[r]
+                if (row[cur_col_index] != 0):
+                    swap(cur_col_index, r)
+                    break
+                else cur_col_index += 1
+        return cur_col_index
 
 
-def move_to_last(matrix, x):
-    """
-    move the selected row x to the last of the matrix
-    """
-    move_to_last = [matrix[x]]
-    del matrix[x]
-    matrix = matrix + move_to_last
-    return matrix
+# mtx1.swap(0, 1) will mutate mtx1 to be
+# [[3, 4], [1, 2]]
+    def swap(self, row1, row2):
+        if (row1 < self.n and row2 < self.n):
+            if (row1 != row2):
+                newrow1 = self.values[row2]
+                newrow2 = self.values[row1]
+                self.values[row1] = newrow1
+                self.values[row2] = newrow2
+        else:
+            raise Exception('Col mismatch')
 
-def scale(row):
-    """
-    scale a given row so the leading entry is 1
-    """
-    factor = row[0]
-    if factor != 1:
-        row = [x / float(factor) for x in row]
-    return row
+# mtx1.scale(0, 1) will mutate mtx1 to be
+# [[0.5, 1], [3, 4]]
+# Note: If will throw an exception if the element to scale by is 0 or if the row or the col are out of bound
+    def scale(self, row, col):
+        if (row < self.m and col < self.n and self.values[row][col] != 0):
+            rowValues = self.values[row]
+            self.values[row] = map(lambda x : x / float(rowValues[col]), rowValues)
+        else:
+            raise Exception('Cannot scale')
 
-def reduce_to_zero(row1, row2):
-    """
-    reduces row2 with a leading entry in a column
-    to the right of that of row1
-    """
-    i = 0
-    while (row2[i]== 0):
-        i += 1
-    factor = row2[i];
-    row1 = [x * factor for x in row1]
-    row2 = map(operator.sub, row2, row1)
-    return row2
-
-def reduce(row, matrix):
-    """
-    given a specified row, row reduce the entry
-    below to 0
-    """
-    reduced_matrix = []
-    for y in matrix[row+1:]:
-        first_row = scale(matrix[row])
-        reduced_matrix = first_row
-        reduced_matrix += reduce_to_zero(matrix[row],y)
-    return reduced_matrix
-
-print (row_reduce([[0, 3, -6, 6, 4, -5],
-                    [3, -9, 12, -9, 6, 15],
-                    [3, -7, 8, -5, 8, 9]]))
+    def make_zero(self, row, col):
